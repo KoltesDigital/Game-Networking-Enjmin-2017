5694b96b7f370888e47a4cfb03195f62ccdaae73
Jonathan Giroux <giroux.jo@gmail.com>
2018-11-20 12:10:27 +0100

    Fix rooms

--- servers/game/server.js
+++ servers/game/server.js
@@ -5,40 +5,58 @@ const io = require('socket.io')(http);
 const gameObjectPositionStates = {};
 
 io.on('connection', (socket) => {
-	for (let otherClientId in gameObjectPositionStates) {
-		const clientGameObjectPositionStates = gameObjectPositionStates[otherClientId];
-		for (let gameObjectId in clientGameObjectPositionStates) {
-			io.emit('create-game-object', otherClientId, gameObjectId, clientGameObjectPositionStates[gameObjectId]);
-		}
-	}
-
-	const ownGameObjectPositionStates = gameObjectPositionStates[socket.id] = {};
+	let gameObjectPositionStatesInRoom;
+	let ownGameObjectPositionStates;
+	let channel = null;
 
 	socket.on('disconnect', () => {
+		if (!channel) return;
 		for (let gameObjectId in ownGameObjectPositionStates) {
-			io.emit('destroy-game-object', socket.id, gameObjectId);
+			io.to(channel).emit('destroy-game-object', socket.id, gameObjectId);
 		}
-		delete gameObjectPositionStates[socket.id];
+		delete gameObjectPositionStatesInRoom[socket.id];
 	});
 
 	socket.on('join', (name) => {
+		if (channel)
+			socket.leave(channel);
+
+		if (!name) return;
+
 		socket.join(name);
+		channel = name;
+
+		gameObjectPositionStatesInRoom = gameObjectPositionStates[channel];
+		if (!gameObjectPositionStatesInRoom)
+			gameObjectPositionStatesInRoom = gameObjectPositionStates[channel] = {};
+
+		for (let otherClientId in gameObjectPositionStatesInRoom) {
+			const clientGameObjectPositionStates = gameObjectPositionStatesInRoom[otherClientId];
+			for (let gameObjectId in clientGameObjectPositionStates) {
+				io.emit('create-game-object', otherClientId, gameObjectId, clientGameObjectPositionStates[gameObjectId]);
+			}
+		}
+
+		ownGameObjectPositionStates = gameObjectPositionStatesInRoom[socket.id] = {};
 	});
 
 	socket.on('create-game-object', (gameObjectId, positionState) => {
+		if (!channel) return;
 		ownGameObjectPositionStates[gameObjectId] = positionState;
-		io.emit('create-game-object', socket.id, gameObjectId, positionState);
+		io.to(channel).emit('create-game-object', socket.id, gameObjectId, positionState);
 	});
 
 	socket.on('destroy-game-object', (gameObjectId) => {
+		if (!channel) return;
 		delete ownGameObjectPositionStates[gameObjectId];
 
-		io.emit('destroy-game-object', socket.id, gameObjectId);
+		io.to(channel).emit('destroy-game-object', socket.id, gameObjectId);
 	});
 
 	socket.on('synchronize-position', (gameObjectId, positionState) => {
+		if (!channel) return;
 		ownGameObjectPositionStates[gameObjectId] = positionState;
-		io.emit('synchronize-position', socket.id, gameObjectId, positionState);
+		io.to(channel).emit('synchronize-position', socket.id, gameObjectId, positionState);
 	});
 });
 
